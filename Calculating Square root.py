3cc0c5049929e1f5f75ac3e1bbc6064ec832c44d
Ali Waqar <ali.waqar0328@gmail.com>
2026-01-13 19:37:49 +0500

    Add files via upload

---
+++ Calculating Square root.py
@@ -0,0 +1,6 @@
+# Calculating the square root using math module
+
+user_input = int(input("Enter the number: "))
+import math
+sq_root = math.sqrt(user_input)
+print(f"The square root of {user_input} is {sq_root}")
